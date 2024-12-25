2b2500de1f838aff1538436fb134102931a1fe5e
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-25 21:48:09 +0500

    7-misol.py bajarildi

--- 7-misol.py
+++ 7-misol.py
@@ -1,3 +1,3 @@
-matn = input("matn kiriting: ")
+matn = input("matn kiriting: ")#matn nomli obyectiga qiymat ta`minlaymiz
 
-print("python" in matn)
+print("python" in matn)#'python' so`zi biz kiritgan matn ichida bor yo`qligini tekshirib e`lon qilish 

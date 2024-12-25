2db6c35fc3d3f967e69602afac816157b58bbee7
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-25 21:41:00 +0500

    4-misol.py bajarildi

--- 4-misol.py
+++ 4-misol.py
@@ -1,6 +1,6 @@
-age = int(input("Yoshingizni kiriting: "))
+age = int(input("Yoshingizni kiriting: "))#age obyektiga int turiga mansub son ta`minladik
 
-if age > 18: 
+if age > 18: #agar age obyektimizni qiymati 18 dan katta bo`lsa
     print("Bolam ado bo`libsanku, voyaga ham etibsan. Endi uylantiramiz seni" )
 else:
-    print("hali patsancha ekansanku")    
+    print("hali patsancha ekansanku")

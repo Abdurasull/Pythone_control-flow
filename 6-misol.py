0de74e34cb095fe81af57dad8b59fbea1b6248e5
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-25 21:44:43 +0500

    6-misol.py bajarildi

--- 6-misol.py
+++ 6-misol.py
@@ -1,3 +1,3 @@
-matn = input("matn kiriting: ")
+matn = input("matn kiriting: ")#matn nomli obyectiga qiymat ta`minlaymiz
 
-print(matn.title())
+print(matn.title())#barja so`zlarni bosh harfini kattaga o`zgartirib e`lon qilamiz

f607bdd3a1ff63add4f7930c438c001714c665f5
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-26 04:42:26 +0500

    9-misol.py bajarildi

--- 3-misol.py
+++ 3-misol.py
@@ -2,4 +2,4 @@ matn = input("Jumla kiriting: ")#matn nomli obyectiga qiymat ta`minlaymiz
 
 Count_Alpha = matn.count('a')#a harfi matn obyektimizda nechta borligini aniqladik va uni Count_Alpha nomli obyektimizga ta`minladik`
 
-print("result: ", Count_Alpha)#natijani e`lon qildik`
+print("result: ", Count_Alpha)#natijani e`lon qildik
--- 9-misol.py
+++ 9-misol.py
@@ -1,7 +1,7 @@
-number = int(input("Sonni kiriting: "))
+number = int(input("Sonni kiriting: "))#number obyektiga int turida qiymat taminlaymiz
 
-s = 0
-while number:
-    s+=number
-    number-=1
-print("result {} ".format(s))
+s = 0#boshlang`ich qiymati 0 ga ting bolgan S nomli obyekt yaratib olamiz
+while number:#while loop orqali o`zigacha bo`lgan barcha sonlarni yig`indisini hisoblaymiz
+    s+=number#S nomli obyektimizga numberni boshlang`ich qiymatidan boshlab qo`shib boramiz
+    number-=1#har bitta sikilimizda number ni 1 ga kamaytirib boramiz
+print("result {} ".format(s))#va loop to`gagandan kn natijani ekranga chop etamiz

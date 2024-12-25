0aa98b6e62c65510271ec85b3eb46de70688918b
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-25 21:28:36 +0500

    2-misol.py bajarildi

--- 2-misol.py
+++ 2-misol.py
@@ -2,4 +2,4 @@ matn = input("Jumla kiriting:")#matn nomli obyectiga qiymat ta`minlaymiz
 
 count = len(matn.split())#matndagi barcha so`zlarini split() methodi yordamida bir o`chamli massiv elementlari sifatida taminlab oldik va len() functioni yordamida ularni sonini  count obyektiga taminladik
 
-print("So`zlar soni {} ta :)".format(count))#natijani esa e`lon qildik
+print("So`zlar soni {} ta :)".format(count))#natijani esa e`lon qildik 

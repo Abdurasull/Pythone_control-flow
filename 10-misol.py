04c6bb8287a7b31cc4b60f0eef7963511701dc19
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-26 04:53:45 +0500

    10-misol.py bajarildi

--- 10-misol.py
+++ 10-misol.py
@@ -1,10 +1,10 @@
-matn = input("Matinni kiriting: ")
+matn = input("Matinni kiriting: ")#matn nomli obyektimizni qiymat bn ta`minlaymiz
 
-matn_array = matn.split()
+matn_array = matn.split()#matn_array nomli massiv yaratib olamiz va unga split function orqali matn obyektimizga tegizli bo lgan barcha so`zlarni massiv ni elementlari sifatida qabul qilamiz
 
-max_array = matn_array[0]
+max_array = matn_array[0]#max_array nomli obyekt yaratib olamiz va uning qiymatini massivimizning 0 - chi indexining qiymatiga tenglaymiz 
 
-for i in matn_array:
+for i in matn_array:#for loop orqali eng uzun so`zni aniqlaymiz
     if len(max_array) < len(i):
         max_array = i
-print("result: {}".format(max_array))
+print("result: {}".format(max_array))#va natijani ekranga chiqaramiz

220b25e91631e11620593b4246c6f126d2e0388d
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-25 21:31:06 +0500

    3-misol.py bajarildi

--- 3-misol.py
+++ 3-misol.py
@@ -1,5 +1,5 @@
-matn = input("Jumla kiriting: ")
+matn = input("Jumla kiriting: ")#matn nomli obyectiga qiymat ta`minlaymiz
 
-Count_Alpha = matn.count('a')
+Count_Alpha = matn.count('a')#a harfi matn obyektimizda nechta borligini aniqladik va uni Count_Alpha nomli obyektimizga ta`minladik`
 
-print("result: ", Count_Alpha)
+print("result: ", Count_Alpha)#natijani e`lon qildik`

97b114be282551522279aef6eb50841531e214c9
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-25 21:53:51 +0500

    8-misol.py bajarildi

--- 8-misol.py
+++ 8-misol.py
@@ -1,5 +1,5 @@
-matn = input("matn ni kiriting: ")
+matn = input("matn ni kiriting: ")#matn nomli obyectiga qiymat ta`minlaymiz
 
-array = matn.split()
-for i in range(len(array)):
+array = matn.split()#matndagi barcha so`zlarini split() methodi yordamida bir o`chamli massiv elementlari sifatida taminlab oldik va ularni  array taminladik
+for i in range(len(array)):#arraydagi barcha elementlarni yangi qatordan elon qilish 
     print(array[i])

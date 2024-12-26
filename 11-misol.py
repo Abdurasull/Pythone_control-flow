edd759aa93be0b1ecf5591dcc6c1604074a9e733
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-26 05:01:14 +0500

    11-misol.py bajarildi

--- 11-misol.py
+++ 11-misol.py
@@ -1,3 +1,3 @@
-word_1 = input("1-so`zni kiriting:")
+word_1 = input("1-so`zni kiriting:")#word_1 nomli obyekt yaratamiz va unga qiymat ta`minlaymiz
 
-print(word_1[::-1] == word_1)
+print(word_1[::-1] == word_1)#string slicing yordamida so`zimizni teskari o`qib olamiz va tastlabki qiymati bn solishtirib ekranga chiqaramiz

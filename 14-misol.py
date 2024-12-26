7961f3a83e32f6df9638042fd18cf37c6b3fc503
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-26 05:32:30 +0500

    14-misol.py bajarildi

--- 14-misol.py
+++ 14-misol.py
@@ -1,5 +1,5 @@
-number = int(input("sonni kiriting:"))
+number = int(input("sonni kiriting:"))#number obyektiga butun qiymat ta`minlaymiz
 
-for i in range(1, number + 1):
-    if number % i == 0:
+for i in range(1, number + 1):#for loop orqali 1dan boshlab numberga cha bo`lgan barcha qiymatlarni aylantiramiz`
+    if number % i == 0:#agar i numberga bo`linsa u holda i ni ekranga chiqaramiz`
         print(i, end=" ")
